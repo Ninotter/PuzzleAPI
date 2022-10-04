944d4f9fe8bf70c758690db03b53db0abf9850eb
Nino <nino.didier@live.fr>
2022-10-04 12:42:09 +0200

    delete produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
-use App\Repository\ProduitRepository;
-use Doctrine\Migrations\Configuration\Migration\JsonFile;
-use App\Entity\Produit;
-use Doctrine\ORM\EntityManager;
 use JsonSerializable;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Produit;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\Migrations\Configuration\Migration\JsonFile;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 
 class ProduitController extends AbstractController
@@ -70,18 +70,34 @@ class ProduitController extends AbstractController
         return new JsonResponse($produitJson, Response::HTTP_OK, [], true);
     }
 
+    // /**
+    //  * Supprime un produit
+    //  *
+    //  * @param SerializerInterface $serializer
+    //  * @param ProduitRepository $product
+    //  * @return JsonResponse
+    //  */
+    // #[Route('/produit/delete/{idProduit}', name: 'produit.delete', methods: ['DELETE'])]
+    // #[ParamConverter("produit", options : ["id" => "idProduit"])]
+    // public function deleteProduit(Produit $produit, EntityManager $entityManager): JsonResponse
+    // {
+    //     $entityManager->remove($produit);
+    //     $entityManager->flush();
+    //     return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    // }
+
     /**
-     * Supprime un produit
+     * Met le statut d'un produit à 0
      *
      * @param SerializerInterface $serializer
      * @param ProduitRepository $product
      * @return JsonResponse
      */
-    #[Route('/produit/delete/{idProduit}', name: 'produit.delete', methods: ['DELETE'])]
+    #[Route('/produit/{idProduit}', name: 'produit.delete', methods: ['DELETE'])]
     #[ParamConverter("produit", options : ["id" => "idProduit"])]
-    public function deleteProduit(Produit $produit, EntityManager $entityManager): JsonResponse
+    public function deleteProduit(Produit $produit, EntityManagerInterface $entityManager): JsonResponse
     {
-        $entityManager->remove($produit);
+        $produit->setStatus(false);
         $entityManager->flush();
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
